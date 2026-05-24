ba6b3c2f5349c69e2579d5d71770888f08f68e33
L3MonKe <hhcrystal2008@gmail.com>
2026-05-24 23:45:01 +0800

    Epsilon 2026.5.2

--- common/src/main/java/com/github/epsilon/utils/rotation/RotationUtils.java
+++ common/src/main/java/com/github/epsilon/utils/rotation/RotationUtils.java
@@ -351,13 +351,6 @@ public class RotationUtils {
             }
         }
 
-        /*
-         * Fixing AimModulo360
-         */
-        if (yaw < 360.0f && yaw > -360.0f) {
-            yaw = yaw + 720.0f;
-        }
-
         return new Rot2f(yaw, pitch);
     }
 
